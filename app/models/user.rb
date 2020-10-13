fdd1b3104be4189c70c99170177cf67f052e3ee8
Zach_Kobrinsky <zachkobrinsky@gmail.com>
2020-10-13 14:57:59 -0400

    added user validations

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,9 @@
 class User < ApplicationRecord
     has_secure_password
+    validates :username, presence: true
+    validates :username, uniqueness: true
+    validates :email, presence: true
+    validates :email, uniqueness: true
 
     has_many :sent_gift_cards, class_name: "GiftCard", foreign_key: "sender_id"
     has_many :received_gift_cards, class_name: "GiftCard", foreign_key: "recipient_id"
