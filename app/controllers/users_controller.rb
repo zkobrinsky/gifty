4d6ce0c2eaf43de3c91e6b30b747263ffe4baa1d
Zach_Kobrinsky <zachkobrinsky@gmail.com>
2020-10-13 15:58:48 -0400

    formatting

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,17 +1,14 @@
 class UsersController < ApplicationController
   def new
-    # byebug
     @user = User.new
   end
 
   def create
     @user = User.new(user_params)
-    # byebug
     if @user.save
       session[:user_id] = @user.id
       redirect_to '/welcome'
     else
-      # byebug
       flash[:alert] = @user.errors.full_messages
       render 'users/new'
     end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,9 @@
 class User < ApplicationRecord
+    has_many :sent_gift_cards, class_name: "GiftCard", foreign_key: "sender_id"
+    has_many :received_gift_cards, class_name: "GiftCard", foreign_key: "recipient_id"
+
+    scope :admins, -> {where(admin:true)}
+
     has_secure_password
     validates :password, length: { minimum: 6, maximum: 20 }
 
@@ -7,11 +12,6 @@ class User < ApplicationRecord
     validates :email, presence: true
     validates :email, uniqueness: true
     validates_format_of :email, :with => /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\z/i
-
-    has_many :sent_gift_cards, class_name: "GiftCard", foreign_key: "sender_id"
-    has_many :received_gift_cards, class_name: "GiftCard", foreign_key: "recipient_id"
-
-    scope :admins, -> {where(admin:true)}
 
     def stores
         giftcards = (self.sent_gift_cards + self.received_gift_cards).uniq
