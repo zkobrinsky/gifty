1c919969afc33fd5ff1ac4331b5157eefc47d52c
Zach_Kobrinsky <zachkobrinsky@gmail.com>
2020-10-14 11:27:51 -0400

    added render and :current_user to gccontroller

--- app/controllers/gift_cards_controller.rb
+++ app/controllers/gift_cards_controller.rb
@@ -1,7 +1,14 @@
 class GiftCardsController < ApplicationController
     before_action :require_login
+    before_action :current_user
 
     def index
+        # current_user
+        # byebug
+        render 'giftcards/index'
+    end
+
+    def show
 
     end
 
