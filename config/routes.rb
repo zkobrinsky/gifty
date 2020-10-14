448c38bce64f1ba3b7e028a67b2f312e4baee961
Zach_Kobrinsky <zachkobrinsky@gmail.com>
2020-10-14 09:59:50 -0400

    added destroy route

--- config/routes.rb
+++ config/routes.rb
@@ -3,5 +3,6 @@ Rails.application.routes.draw do
    get 'login', to: 'sessions#new'
    post 'login', to: 'sessions#create'
    get 'welcome', to: 'sessions#welcome'
+   delete 'sessions', to: 'sessions#destroy'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
