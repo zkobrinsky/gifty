ed50b1df52a618a78141448f27dd352a9fc1baae
Zach_Kobrinsky <zachkobrinsky@gmail.com>
2020-10-14 11:27:08 -0400

    remoed extra "path" from routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,9 @@
 Rails.application.routes.draw do
   resources :users, only: [:new, :create] do
-    resources :gift_cards, only: [:index, :new, :create]
+    resources :gift_cards, only: [:index, :new, :create, :show]
   end
-  get 'users/:user_id/gift_cards/received', to: 'gift_cards#received', as: 'user_received_giftcards_path'
-  get 'users/:user_id/gift_cards/sent', to: 'gift_cards#sent', as: 'user_sent_giftcards_path'
+  get 'users/:user_id/gift_cards/received', to: 'gift_cards#received', as: 'user_received_giftcards'
+  get 'users/:user_id/gift_cards/sent', to: 'gift_cards#sent', as: 'user_sent_giftcards'
   get 'login', to: 'sessions#new'
   post 'login', to: 'sessions#create'
   get 'welcome', to: 'sessions#welcome'
