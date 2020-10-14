758061e3fd8a09e13ebc4d4b696f0f72419f4452
Zach_Kobrinsky <zachkobrinsky@gmail.com>
2020-10-14 10:50:08 -0400

    refactored nested gift_card routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,9 @@
 Rails.application.routes.draw do
   resources :users, only: [:new, :create] do
-    resources :giftcards, only: [:index, :new, :create]
+    resources :gift_cards, only: [:index, :new, :create]
   end
-  get 'users/:user_id/giftcards/received', to: 'giftcards#received', as: 'user_received_giftcards_path'
-  get 'users/:user_id/giftcards/sent', to: 'giftcards#sent', as: 'user_sent_giftcards_path'
+  get 'users/:user_id/gift_cards/received', to: 'gift_cards#received', as: 'user_received_giftcards_path'
+  get 'users/:user_id/gift_cards/sent', to: 'gift_cards#sent', as: 'user_sent_giftcards_path'
   get 'login', to: 'sessions#new'
   post 'login', to: 'sessions#create'
   get 'welcome', to: 'sessions#welcome'
