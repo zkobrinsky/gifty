8d75c2800b528b259aa3ab1827cf157bda601e81
Zach_Kobrinsky <zachkobrinsky@gmail.com>
2020-10-14 09:54:44 -0400

    added helpers to apcontroller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,10 +3,20 @@ class ApplicationController < ActionController::Base
     helper_method :logged_in?
 
     def current_user
-        User.find_by(id: session[:user_id])
+        @current_user ||= User.find(session[:user_id]) if session[:user_id]
     end
 
     def logged_in?
         !!current_user
     end
+
+    def log_in
+        session[:user_id] = @user.id
+    end
+
+    def require_login
+        unless current_user
+          redirect_to login_path, flash[:alert] => "Please log in to access this page."
+        end
+      end
 end
