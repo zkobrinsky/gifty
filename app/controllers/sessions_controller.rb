62bb2b910b7c8721499a5520135978cbba777ef9
Zach_Kobrinsky <zachkobrinsky@gmail.com>
2020-10-14 09:57:35 -0400

    added create and destroy to sessions

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -3,11 +3,23 @@ class SessionsController < ApplicationController
   end
 
   def create
-  end
-
-  def login
-  end
+    @user = User.find_by(username: params[:username])
+    byebug
+    if @user && @user.authenticate(params[:password])
+       log_in
+       redirect_to '/welcome'
+    else
+      @user.valid?
+      flash[:alert] = @user.errors.full_messages
+      redirect_to login_path
+    end
+ end
 
   def welcome
+  end
+
+  def destroy
+    session.clear
+    redirect_to '/welcome'
   end
 end
