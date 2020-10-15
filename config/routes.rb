1ebea91e8e08815c659249c48dc5c12c36f15889
Zach_Kobrinsky <zachkobrinsky@gmail.com>
2020-10-15 16:49:36 -0400

    added edit, update, show routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   get 'users/:user_id/gift_cards/sent', to: 'gift_cards#sent', as: 'user_sent_gift_cards'
 
-  resources :users, only: [:new, :create] do
+  resources :users, only: [:new, :create, :edit, :update, :show] do
     resources :gift_cards, only: [:index, :new, :create, :show]
   end
   get '/auth/facebook/callback' => 'sessions#create'
