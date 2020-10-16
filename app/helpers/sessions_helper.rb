06efc39199a690c943183765209f38e756cf9bee
Zach_Kobrinsky <zachkobrinsky@gmail.com>
2020-10-16 17:25:16 -0400

    show_time and username_or_email helpers

--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -1,2 +1,9 @@
 module SessionsHelper
+    def show_time(card)
+        card.created_at.localtime.strftime('%b %e,%l:%M %p')
+    end
+
+    def username_or_email(user)
+        user.username ? user.username : user.email
+    end
 end
