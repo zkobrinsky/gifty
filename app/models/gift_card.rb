ffa9379c7fc59ff109ea4718b2f86df62265809c
Zach_Kobrinsky <zachkobrinsky@gmail.com>
2020-10-16 12:40:32 -0400

    validates dollar value

--- app/models/gift_card.rb
+++ app/models/gift_card.rb
@@ -2,6 +2,7 @@ class GiftCard < ApplicationRecord
     belongs_to :sender, class_name: "User", foreign_key: "sender_id"
     belongs_to :recipient, class_name: "User", foreign_key: "recipient_id"
     belongs_to :store
+    validates :dollar_value, length: { in: 1..11 }
 
     scope :big_spender, -> {where("dollar_value >= ?", 1000)}
 
