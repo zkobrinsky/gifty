4262088277008721d7b13890cd05486f7e0980bb
Zach_Kobrinsky <zachkobrinsky@gmail.com>
2020-10-26 09:25:11 -0400

    refactored Store.users, added senders and recipients

--- app/models/store.rb
+++ app/models/store.rb
@@ -2,10 +2,17 @@ class Store < ApplicationRecord
     has_many :gift_cards
 
     def users
-        giftcards = GiftCard.where(store_id: self.id)
-        senders = giftcards.pluck(:sender_id)
-        recipients = giftcards.pluck(:recipient_id)
+        senders = self.gift_cards.pluck(:sender_id)
+        recipients = self.gift_cards.pluck(:recipient_id)
         User.find((recipients+senders).uniq)
+    end
+
+    def senders
+        User.find(self.gift_cards.pluck(:sender_id))
+    end
+
+    def recipients
+        User.find(self.gift_cards.pluck(:recipient_id))
     end
 end
 
