23fb089759d1c4d5cb44ba131df70086a523449c
Zach_Kobrinsky <zachkobrinsky@gmail.com>
2020-10-27 08:44:30 -0400

    commented out public show

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   get 'users/:user_id/gift_cards/sent', to: 'gift_cards#sent', as: 'user_sent_gift_cards'
 
-  get 'gift_cards/:id', to: 'gift_cards#public_show', as: 'gift_card'
+  # get 'gift_cards/:id', to: 'gift_cards#public_show', as: 'gift_card'
 
   resources :users, only: [:new, :create, :edit, :update, :show] do
     resources :gift_cards, only: [:index, :new, :create, :show]
